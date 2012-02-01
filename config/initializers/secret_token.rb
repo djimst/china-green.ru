56233c6cc82700517047ad226761f35e07c51198
djimst <jimst@mail.ru>
2012-02-01 14:56:08 +0300

    Vse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChinaGreenRu::Application.config.secret_token = '8205afc65c350c22600a29a8aed1df482e0398074276874307b04c98d6341f6088bf69ea3f73350b6ef9f46c696fe899bad499e39ad8e8a7f3d722a86b5fb8d6'
